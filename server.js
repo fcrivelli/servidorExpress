6068f5fff353ed7cc2bd76e3323eeb1d2956a521
fcrivelli <cofrta@gmail.com>
2021-08-19 23:18:02 -0300

    Upload Server Express

---
+++ server.js
@@ -0,0 +1,184 @@
+var indice = 0;
+var mapProduct = new Map();
+const express = require('express');
+const PORT = 8080;
+const api = express();
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+var $ = jQuery = require('jquery')(window);
+
+const handlebars = require('express-handlebars');
+const msjProductNotFound = {error: "producto no encontrado"}
+const msjProductsNotCharge = {error: "no hay productos cargados"}
+const msjError = {error: "error en la petición"}
+const msjDelete = {success: "producto borrado"}
+const msjSave = { success: "producto fue guardado"}
+
+var workProducts = (function() {
+    var BUTTON_SAVE_ID = '#buttonSave';
+    var TITLE_ID = '#inputTitle';
+    var PRICE_ID = '#inputPrice';
+    var THUMBNAIL_ID = '#inputThumbnail';
+
+    $(document).success(function(){
+        $(BUTTON_SAVE_ID).on('click', function(){
+            onClickButtonSave();
+        })
+    })
+
+    var saveProduct = function(product) {
+        mapProduct[indice] = product;
+        indice++;
+    }
+
+    var deleteProduct = function(product) {
+        mapProduct.delete(product.id);
+    }
+
+    var updateProduct = function(product) {
+        for (var [key, value] of mapProduct){
+            if(key === product.id){
+                value.title = product.title;
+                value.price = product.price;
+                value.thumbnail = product.thumbnail;
+                break;
+            }
+        }
+    }
+    
+    var getProductById = function(key) {
+        return mapProduct[key];
+    }
+    
+    var getProducts = function() {
+        var listProduct = [];
+        Object.keys(mapProduct).forEach((key) => {
+            listProduct.push({
+                id: key,
+                title: mapProduct[key].title,
+                price: mapProduct[key].price,
+                thumbnail: mapProduct[key].thumbnail
+            });
+        });
+        return listProduct;
+    }
+    
+    var validateBody = function (product) {
+        return product != null;
+    }
+    
+    var validateId = function (id) {
+        return id && mapProduct[id];
+    }
+
+    var onClickButtonSave = function() {
+        if(validateForm()){
+            saveProduct({
+                title: $(TITLE_ID)[0].value,
+                price: $(PRICE_ID)[0].value,
+                thumbnail: $(THUMBNAIL_ID)[0].value
+            });
+        }
+    };
+
+    var validateForm = function() {
+        let validate = true;
+        if ($(TITLE_ID)[0].value.length == 0) {
+            validate = false;
+        }
+        if ($(PRICE_ID)[0].value.length == 0) {
+            validate = false;
+        }
+        if ($(THUMBNAIL_ID)[0].value.length == 0) {
+            validate = false;
+        }
+        if(!validate) alert("Completar todos los campos");
+        return validate;
+    }
+
+    return {
+        onClickButtonSave: onClickButtonSave,
+        saveProduct: saveProduct,
+        getProductById: getProductById,
+        getProducts: getProducts,
+        validateBody: validateBody,
+        validateId: validateId,
+        updateProduct: updateProduct,
+        deleteProduct: deleteProduct
+    }
+
+})();
+
+api.engine('hbs', handlebars({
+    extname: ".hbs",
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + '/views/partials/'
+}));
+api.set('view engine', 'hbs');
+api.set('views', './views');
+api.use(express.urlencoded({extended: true}));
+api.use(express.static('public'));
+
+api.get('/', (req, res) => {
+    res.render("./layouts/addProduct", {});
+})
+
+api.get('/productos/vista', (req, res) => {
+    res.render("main", { suggestedChamps: workProducts.getProducts(), listExists: true });
+})
+
+api.get('/api/productos/listar', (req, res) => {
+    if(indice == 0){
+        res.status(400).send(msjProductsNotCharge);
+    } else {
+        res.send(workProducts.getProducts());
+    }
+});
+
+api.get('/api/productos/listar/:id', (req, res) => {
+    if(!workProducts.validateId(req.params.id)){
+        res.status(400).send(msjProductNotFound);
+    } else {
+        res.send({
+            id: req.params.id,
+            product: workProducts.getProductById(req.params.id)
+        });
+    }
+});
+
+api.delete('/api/productos/borrar', (req, res) => {
+    if(!workProducts.validateBody(req.body)){
+        res.status(400).send(msjError);
+    } else {
+        workProducts.deleteProduct(req.body);
+        res.send(msjDelete);
+    }
+});
+
+api.put('/api/productos/actualizar', (req, res) => {
+    if(!workProducts.validateBody(req.body)){
+        res.status(400).send(msjError);
+    } else {
+        workProducts.updateProduct(req.body);
+        res.send(msjSave);
+    }
+});
+
+api.post('/api/productos/guardar', (req, res) => {
+    if(!workProducts.validateBody(req.body)){
+        res.status(400).send(msjError);
+    } else {
+        workProducts.saveProduct(req.body);
+        res.send(msjSave);
+    }
+});
+
+api.listen(PORT, err => {
+    if(err) throw new Error(`Error en servidor ${err}`)
+    console.log("My HTTP server listening on port " + PORT + "...");
+});
